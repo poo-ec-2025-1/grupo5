5c927e5b648f109f7eb5bf9659f9d0f581416dbc
Rodrigo Frota <abdiastelles@Mac-RFT.local>
2025-06-27 18:36:53 -0300

    Importando arquivos do repositório RFT_POO

---
+++ Planta.java
@@ -0,0 +1,64 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import com.j256.ormlite.table.DatabaseTable;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.field.DataType;
+
+@DatabaseTable(tableName = "planta")
+public class Planta
+{
+    @DatabaseField(generatedId = true)
+    private int id;
+
+    @DatabaseField
+    private String nome;
+
+    @DatabaseField
+    private String tipo;
+
+    @DatabaseField
+    private double preco;
+
+    @DatabaseField(dataType = DataType.DATE)
+    private Date dataCadastro;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getPreco() {
+        return this.preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public Date getDataCadastro() {
+        return this.dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+}
