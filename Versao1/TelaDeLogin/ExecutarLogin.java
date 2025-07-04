68194991d1d5b9d3245a324521f92270234c5898
MillenaPimentel <millenacpimentel@gmail.com>
2025-07-04 15:01:07 -0300

    Delete Versao1/TelaDeLogin directory

--- Versao1/TelaDeLogin/ExecutarLogin.java
+++
@@ -1,6 +0,0 @@
-public class ExecutarLogin {
-    public static void main(String[] args) throws Exception {
-        LoginController controller = new LoginController();
-        controller.iniciar();
-    }
-}
